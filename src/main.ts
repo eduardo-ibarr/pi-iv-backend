fc89f1fd6e70c6e13d705cf8e8ad51eadfc10e1c
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-09-02 20:05:37 -0300

    feat: add cors, global prefix and helmet for headers

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+  });
+
+  app.setGlobalPrefix('api/v1');
+  app.use(helmet());
+
   await app.listen(3000);
 }
 bootstrap();
