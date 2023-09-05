7ae09b39d9d052dd14101599df10faaf593143aa
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-09-05 19:40:33 -0300

    refactor: removing some exceptions

--- src/exceptions/mqtt/index.ts
+++ src/exceptions/mqtt/index.ts
@@ -1,6 +1,4 @@
-export { MqttLoadSubscriptionsError } from './mqttLoadSubscriptionsError';
 export { MqttPublishError } from './mqttPublishError';
 export { MqttSubscriptionError } from './mqttSubscriptionError';
-export { MqttUpdateSubscriptionsError } from './mqttUpdateSubscriptionsError';
 export { MqttConnectionError } from './mqttConnectionError';
 export { MqttUnsubscribeError } from './mqttUnsubscribeError';
