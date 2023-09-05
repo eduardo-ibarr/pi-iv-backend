1d0c56bd0f11717ed7bd9b58cdd09af384a7871c
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-09-05 19:42:21 -0300

    refactor: subscriptions moved to a object

---
+++ src/config/constants/subscriptions.ts
@@ -0,0 +1,6 @@
+export const subscriptions = {
+  temperature: 'sensors/temperature',
+  airMoisture: 'sensors/air-moisture',
+  luminosity: 'sensors/luminosity',
+  soilMoisture: 'sensors/soil-moisture',
+};
