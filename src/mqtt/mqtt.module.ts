9f6ecf98d2a68fdaf7466af4f967cb82a09c25ca
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-09-05 19:41:23 -0300

    refactor: mqtt refactored

--- src/mqtt/mqtt.module.ts
+++ src/mqtt/mqtt.module.ts
@@ -2,13 +2,9 @@ import { Module } from '@nestjs/common';
 import { MqttController } from './mqtt.controller';
 import { MqttService } from './mqtt.service';
 import { ConfigModule } from '@nestjs/config';
-import { PassportModule } from '@nestjs/passport';
-import { AuthService } from 'src/auth/auth.service';
-import { BasicStrategy } from 'src/auth/auth.strategy';
-
 @Module({
-  imports: [ConfigModule, PassportModule],
+  imports: [ConfigModule],
   controllers: [MqttController],
-  providers: [MqttService, AuthService, BasicStrategy],
+  providers: [MqttService],
 })
 export class MqttModule {}
--- src/mqtt/mqtt.service.ts
+++ src/mqtt/mqtt.service.ts
@@ -1,21 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { readFileSync, writeFileSync } from 'fs';
 import { MqttClient, connect } from 'mqtt';
-import * as path from 'path';
+import { v4 as uuidv4 } from 'uuid';
 import {
   MqttPublishError,
-  MqttLoadSubscriptionsError,
   MqttSubscriptionError,
-  MqttUpdateSubscriptionsError,
   MqttConnectionError,
   MqttUnsubscribeError,
 } from 'src/exceptions/mqtt';
 
 @Injectable()
 export class MqttService {
+  private readonly logger = new Logger(MqttService.name);
   private client: MqttClient;
-  private savedSubscriptions: string[] = [];
   private envs: string[] = [
     'mqtt.broker',
     'mqtt.username',
@@ -23,17 +20,12 @@ export class MqttService {
     'mqtt.ca',
   ];
 
-  private readonly subscriptionsFile = path.join(
-    __dirname,
-    'subscriptions.json',
-  );
-
   constructor(private configService: ConfigService) {
     const [broker, username, password, ca] = this.envs.map((env) =>
       this.configService.get<string>(env),
     );
 
-    const clientId = `api_nestjs_${Math.random().toString(16).substring(2, 8)}`;
+    const clientId = `api_nestjs_${uuidv4()}`;
 
     this.client = connect(broker, {
       username,
@@ -44,46 +36,12 @@ export class MqttService {
     });
 
     this.client.on('connect', () => {
-      console.log('Connected to MQTT broker.');
+      this.logger.log('Connected to MQTT broker.');
     });
 
     this.client.on('error', (error) => {
       throw new MqttConnectionError(error.message);
     });
-
-    this.savedSubscriptions = this.loadSubscriptions();
-
-    for (const topic of this.savedSubscriptions) {
-      this.subscribe(topic);
-    }
-  }
-
-  private loadSubscriptions(): string[] {
-    try {
-      const data = readFileSync(this.subscriptionsFile, 'utf8');
-      const subscriptions = JSON.parse(data).subscriptions;
-      return subscriptions;
-    } catch (error) {
-      throw new MqttLoadSubscriptionsError(error.message);
-    }
-  }
-
-  private updateAndSaveSubscriptions(newSubscriptions: string[]): void {
-    const data = {
-      subscriptions: newSubscriptions,
-    };
-
-    try {
-      writeFileSync(
-        this.subscriptionsFile,
-        JSON.stringify(data, null, 2),
-        'utf8',
-      );
-
-      console.log('Subscriptions updated and saved successfully.');
-    } catch (error) {
-      throw new MqttUpdateSubscriptionsError(error.message);
-    }
   }
 
   publish(topic: string, message: string): void {
@@ -91,27 +49,22 @@ export class MqttService {
       if (error) {
         throw new MqttPublishError(topic, error.message);
       } else {
-        console.log(`Message published to topic ${topic}: ${message}`);
+        this.logger.log(`Message published to topic ${topic}: ${message}`);
       }
     });
   }
 
-  subscribe(topic: string): void {
+  subscribe(topic: string, callback: (data: any) => void): void {
     try {
       this.client.subscribe(topic);
-      console.log(`Subscribed to the topic ${topic}.`);
+      this.logger.log(`Subscribed to topic ${topic}.`);
 
-      this.client.on('message', (receivedTopic, message) => {
-        if (receivedTopic === topic) {
-          console.log(`Message received from topic ${topic}: ${message}`);
-          // Faça algo com a mensagem recebida
+      this.client.on('message', (topicReceived, message) => {
+        if (topic === topicReceived) {
+          this.logger.log(`Message received from topic ${topic}...`);
+          callback(message.toString());
         }
       });
-
-      if (!this.savedSubscriptions.includes(topic)) {
-        this.savedSubscriptions.push(topic);
-        this.updateAndSaveSubscriptions(this.savedSubscriptions);
-      }
     } catch (error) {
       throw new MqttSubscriptionError(topic, error.message);
     }
@@ -120,14 +73,7 @@ export class MqttService {
   unsubscribe(topic: string): void {
     try {
       this.client.unsubscribe(topic);
-
-      this.savedSubscriptions = this.savedSubscriptions.filter(
-        (sub) => sub !== topic,
-      );
-
-      this.updateAndSaveSubscriptions(this.savedSubscriptions);
-
-      console.log(`Unsubscribed from topic ${topic}.`);
+      this.logger.log(`Unsubscribed from topic ${topic}.`);
     } catch (error) {
       throw new MqttUnsubscribeError(topic, error.message);
     }
